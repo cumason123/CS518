6a925fddec5546417166d6d1d83f6f13d9c3a9de
Curtis Mason <cumason@bu.edu>
2021-02-20 14:02:36 -0500

    Did q3

--- week3/q3.ml
+++ week3/q3.ml
@@ -1,5 +1,17 @@
 open Int64
 open Float
 open QCheck
+open Printf
 
-Gen.generate1 ~n:10 Gen.int_range 0 100;;
+let print_list x = match (List.iter (printf "%d ") x) with
+  | _ -> printf "\n"
+;;
+let generator = (Gen.list_size (Gen.int_range 5 10) (Gen.int_range 0 100));;
+
+printf "Generate1:\n";;
+print_list (Gen.generate1 generator);;
+printf "\n";;
+
+printf "Generate:\n";;
+List.iter (print_list) (Gen.generate ~n:3 generator);;
+printf "\n";;
