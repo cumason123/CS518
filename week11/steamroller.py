06aa56f515c72bc7ecab6d666eac6a91b1b51e4c
Curtis Mason <cumason@bu.edu>
2021-04-18 06:47:27 -0400

    broken steamroller

--- week11/steamroller.py
+++ week11/steamroller.py
@@ -1,18 +1,90 @@
 from z3 import *
-x, y, z = Ints('x y z u')
+x, y, z, u = Ints('x y z u')
+
+
 def Wolf(x: int):
-    return x == 0
+    return x == 4
+
+
 def Fox(x: int):
-    return x == 1
+    return x == 3
+
+
 def Bird(x: int):
     return x == 2
-def Caterpillar(x: int):
-    return x == 3
+
+
 def Snail(x: int):
-    return x == 4
+    return x == 1
+
+
+def Caterpillar(x: int):
+    return x == 0
+
+
 def Grain(x: int):
-    return x == 5
+    return x == -1
+
+
 def Animal(x: int):
     return Wolf(x) or Fox(x) or Bird(x) or Caterpillar(x) or Snail(x)
+
+
 def Plant(x: int):
     return Grain(x)
+
+
+wolfImply = Implies(Wolf(x), Animal(x))
+foxImply = Implies(Fox(x), Animal(x))
+BirdImply = Implies(Bird(x), Animal(x))
+caterpillarImply = Implies(Caterpillar(x), Animal(x))
+snailImply = Implies(Snail(x), Animal(x))
+grainImply = Implies(Grain(x), Plant(x))
+
+wolfExists = Exists(x, Wolf(x))
+foxExists = Exists(x, Fox(x))
+birdExists = Exists(x, Bird(x))
+snailExists = Exists(x, Snail(x))
+caterpillarExists = Exists(x, Caterpillar(x))
+grainExists = Exists(x, Grain(x))
+
+
+def Smaller(x: int, y: int):
+    return Or(
+        And(Caterpillar(x), Bird(y)),
+        And(Snail(x), Bird(y)),
+        And(Bird(x), Fox(y)),
+        And(Fox(x), Wolf(y)),
+    )
+
+
+def Eats(x: int, y: int):
+    return Or(
+        And(Caterpillar(x), Plant(y)),
+        And(Snail(x), Plant(y)),
+        And(x == 4, y > -1, y < 3),
+        And(Bird(x), Caterpillar(y)),
+        And(Fox(x), y < 3, y > -1)
+    )
+
+
+assumption = Implies(Animal(x), Or(Implies(Plant(y), Eats(x, y)), Implies(
+    And(Animal(z), Smaller(z, x), Exists(u, And(Plant(u), Eats(z, u)))), Eats(x, z))))
+
+
+caterpillarRule = Implies(Caterpillar(x), Exists(y, And(Plant(y), Eats(x, y))))
+snailRule = Implies(Snail(x), Exists(y, And(Plant(y), Eats(x, y))))
+
+wolfFox = Implies(And(Wolf(x), Fox(y)), Not(Eats(x, y)))
+wolfGrain = Implies(And(Wolf(x), Grain(y)), Not(Eats(x, y)))
+birdSnail = Implies(And(Bird(x), Snail(y)), Not(Eats(x, y)))
+
+goal = Exists(x, Exists(y, And(
+    Animal(x),
+    Animal(y),
+    Eats(x, y),
+    Implies(Grain(z), Eats(y, z))
+)))
+s = Solver()
+s.add(goal)
+print(s.check())
